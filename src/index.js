e744b56363511bccdcd9d18708b4f9bc0673c455
Gihan Ubayawardana <gihanubayawardana@gmail.com>
2019-03-22 11:08:58 +0530

    Integrating redux with the application

--- src/index.js
+++ src/index.js
@@ -2,17 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, withRouter   } from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import firebase from 'firebase';
 import 'semantic-ui-css/semantic.min.css'
+import {createStore} from 'redux';
+import {Provider, connect} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import {setUser} from './actions'
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
 
     componentDidMount(){
         firebase.auth().onAuthStateChanged(user => {
             if(user){
+                this.props.setUser(user);
                 this.props.history.push('/');
 
             }
@@ -33,12 +41,15 @@ class Root extends React.Component {
     }
 }
 
-const RootWithAuth = withRouter(Root)
+const RootWithAuth = withRouter(connect(null, {setUser})(Root));
 
 
 ReactDOM.render(
+    <Provider store={store}>
     <Router>
         <RootWithAuth />
-    </Router>,
+    </Router>
+    </Provider>
+    ,
     document.getElementById('root'));
 registerServiceWorker();
